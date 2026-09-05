ee6a2d09ce8f686c7d7787a5e8f9bd13dc2fc708
yifengyou <842056007@qq.com>
2026-09-06 01:02:43 +0800

    cmd: rockusb: Temporarily replace with rbrom
    U-Boot 2026 lacks the Rockchip loader mode code. Temporarily
    replace rockusb with rbrom as a workaround.
    
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ cmd/download.c
@@ -0,0 +1,61 @@
+/*
+ * (C) Copyright 2019 Rockchip Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+
+#include <command.h>
+#include <console.h>
+
+__weak void do_board_download(void)
+{
+}
+
+static int do_download(struct cmd_tbl *cmdtp, int flag,
+		       int argc, char *const argv[])
+{
+	disable_ctrlc(1);
+
+	/* Allow board specific download, maybe noreturn */
+	do_board_download();
+
+#ifdef CONFIG_CMD_USB
+	printf("Download: Scanning USB...\n");
+	run_command("usb start", 0);
+#endif
+
+#ifdef CONFIG_CMD_MMC
+	printf("Download: Scanning EMMC...\n");
+	run_command("mmc rescan", 0);
+	run_command("mmc info", 0);
+#endif
+
+#ifdef CONFIG_CMD_PCI
+	printf("Download: Scanning NVMe...\n");
+	run_command("pci enum", 0);
+#endif
+
+#ifdef CONFIG_CMD_NVME
+	run_command("nvme scan", 0);
+#endif
+
+#ifdef CONFIG_CMD_SCSI
+	printf("Download: Scanning SCSI...\n");
+	run_command("scsi scan", 0);
+#endif
+
+#ifdef CONFIG_CMD_ROCKUSB
+	run_command("rockusb 0 mtd 2", 0);
+#endif
+
+	printf("Enter rockusb failed, fallback to bootrom...\n");
+	flush();
+	run_command("rbrom", 0);
+
+	return 0;
+}
+
+U_BOOT_CMD(
+	download, 1, 0, do_download,
+	"enter rockusb/bootrom download mode", ""
+);
--- cmd/rockusb.c
+++ cmd/rockusb.c
@@ -1,74 +1,72 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2017 Eddie Cai <eddie.cai.linux@gmail.com>
+ * Implements the 'rbrom' command to reboot into Rockchip Maskrom mode
+ *
+ * Copyright (C) 2024 Rockchip Electronics Co., Ltd.
  */
 
 #include <command.h>
-#include <console.h>
-#include <g_dnl.h>
-#include <usb.h>
-#include <asm/arch-rockchip/f_rockusb.h>
+#include <asm/io.h>
+#include <asm/arch-rockchip/boot_mode.h>
+#include <linux/delay.h>
 
-static int do_rockusb(struct cmd_tbl *cmdtp, int flag, int argc,
-		      char *const argv[])
+/* 
+ * Ensure BOOT_BROM_DOWNLOAD is defined. 
+ * Usually found in asm/arch-rockchip/boot_mode.h, 
+ * but defined here as fallback for compatibility.
+ */
+#ifndef BOOT_BROM_DOWNLOAD
+#define BOOT_BROM_DOWNLOAD	0x52424330  /* "RBC0" */
+#endif
+
+static int do_reboot_brom(struct cmd_tbl *cmdtp, int flag, int argc,
+			  char *const argv[])
 {
-	int controller_index, dev_index;
-	char *usb_controller;
-	struct udevice *udc;
-	char *devtype;
-	char *devnum;
-	int ret;
+	u32 reg_val;
 
-	if (argc < 2)
-		return CMD_RET_USAGE;
+	/* 1. Read and print register value before write */
+	reg_val = readl(CONFIG_ROCKCHIP_BOOT_MODE_REG);
+	printf("Boot Mode Reg BEFORE write: 0x%08x to Reg 0x%08x\n",
+	       reg_val, CONFIG_ROCKCHIP_BOOT_MODE_REG);
 
-	usb_controller = argv[1];
-	controller_index = simple_strtoul(usb_controller, NULL, 0);
+	/*
+	 * These SOC_CON1 regs needs to be cleared before a reset or the
+	 * BOOT_MODE_REG do not retain its value and it is not possible
+	 * to reset to bootrom download mode once TF-A has been started.
+	 *
+	 * TF-A blobs for RK3568 already clear SOC_CON1 for PSCI reset.
+	 * However, the TF-A blobs for RK3588 does not clear SOC_CON1.
+	 */
+	if (IS_ENABLED(CONFIG_ROCKCHIP_RK3568))
+		writel(0x40000, 0xFDC20104);
+	if (IS_ENABLED(CONFIG_ROCKCHIP_RK3588))
+		writel(0xFFFF0000, 0xFD58A004);
 
-	if (argc >= 4) {
-		devtype = argv[2];
-		devnum  = argv[3];
-	} else {
-		return CMD_RET_USAGE;
-	}
-	dev_index = simple_strtoul(devnum, NULL, 0);
-	rockusb_dev_init(devtype, dev_index);
+	/* 2. Write the Maskrom download magic */
+	writel(BOOT_BROM_DOWNLOAD, CONFIG_ROCKCHIP_BOOT_MODE_REG);
 
-	ret = udc_device_get_by_index(controller_index, &udc);
-	if (ret) {
-		printf("USB init failed: %d\n", ret);
-		return CMD_RET_FAILURE;
-	}
+	/* 3. Small delay to ensure hardware latches the write */
+	udelay(1000);
 
-	g_dnl_clear_detach();
-	ret = g_dnl_register("usb_dnl_rockusb");
-	if (ret)
-		return CMD_RET_FAILURE;
+	/* 4. Read back and verify the written value */
+	reg_val = readl(CONFIG_ROCKCHIP_BOOT_MODE_REG);
+	printf("Boot Mode Reg AFTER write:  0x%08x\n", reg_val);
+	printf("Expected value:             0x%08x\n", BOOT_BROM_DOWNLOAD);
 
-	if (!g_dnl_board_usb_cable_connected()) {
-		puts("\rUSB cable not detected, Command exit.\n");
-		ret = CMD_RET_FAILURE;
-		goto exit;
-	}
+	if (reg_val == BOOT_BROM_DOWNLOAD)
+		printf("Write SUCCESS! Rebooting to Maskrom...\n");
+	else
+		printf("Write FAILED! Value mismatch.\n");
 
-	while (1) {
-		if (g_dnl_detach())
-			break;
-		if (ctrlc())
-			break;
-		dm_usb_gadget_handle_interrupts(udc);
-	}
-	ret = CMD_RET_SUCCESS;
+	/* 5. Trigger system reset */
+	do_reset(NULL, 0, 0, NULL);
 
-exit:
-	g_dnl_unregister();
-	g_dnl_clear_detach();
-	udc_device_put(udc);
-
-	return ret;
+	/* Should never reach here */
+	return CMD_RET_SUCCESS;
 }
 
-U_BOOT_CMD(rockusb, 4, 1, do_rockusb,
+
+U_BOOT_CMD(rockusb, 4, 1, do_reboot_brom,
 	   "use the rockusb protocol",
 	   "<USB_controller> <devtype> <dev[:part]>  e.g. rockusb 0 mmc 0\n"
 );
